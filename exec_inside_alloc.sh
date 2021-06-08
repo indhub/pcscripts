ee67d4f3b4070abca9ad58af185b7e83fa78b141
Indu <thangakr@amazon.com>
2021-06-08 05:12:00 +0000

    hrun and hbatch

---
+++ exec_inside_alloc.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+  -N | --nodes)
+    num_nodes=$2
+    shift 2
+    ;;
+  --container)
+    container_name=$2
+    shift 2
+    ;;
+  --script_dir)
+    script_dir=$2
+    shift 2
+    ;;
+  *)
+    break
+    ;;
+  esac
+done
+
+export tasks_per_node=8
+export num_tasks=$((tasks_per_node*num_nodes))
+export OMPI_MCA_btl_tcp_if_exclude="docker0,lo"
+export PMIX_MCA_gds=hash
+
+export num_cpus=`srun -N 1 lscpu -p | tail -n 1 | cut -d ',' -f1`
+export num_cpus=$((num_cpus+1))
+export num_cpus_per_task=$((num_cpus/tasks_per_node))
+
+#salloc -N 2 --exclusive --no-shell --wait-all-nodes 1 
+#echo $SLURM_JOB_ID
+#srun echo hello
+#srun echo world
+#exit
+srun nvidia-modprobe -u -c=0
+srun -n $num_tasks --container-name ${container_name} \
+  --container-mount-home --no-container-remap-root --container-workdir $HOME \
+  --container-mounts ${script_dir}:/pcscripts,/scratch:/scratch,/fsx:/fsx \
+  --cpus-per-task ${num_cpus_per_task} --mpi=pmix --unbuffered /pcscripts/runwithenvvars \
+  smddprun -c /opt/conda $@
